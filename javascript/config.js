492e72ad68e359b8e2dba027b42add1cda1bac38
Santiago M <maret.santiago@gmail.com>
2023-10-14 09:15:38 -0300

    Feature 2: Cambios grandes con errores. Todas las paginas tienen html, css, navbar y footer. El home fue completado con su contenido correspondiente, pero queda todo lo que es estilo y estructuras a corregir

--- javascript/config.js
+++ javascript/config.js
@@ -2,3 +2,28 @@ const API_URL = "https://sheetdb.io/api/v1/lmtxsypehe67b";
 
 export { API_URL };
 
+const gallerySlides = document.querySelector('.gallery-slides');
+const galleryImages = document.querySelectorAll('.gallery-image');
+const prevButton = document.getElementById('prev-button');
+const nextButton = document.getElementById('next-button');
+let currentIndex = 0;
+
+function showImage(index) {
+    const offset = -index * 100;
+    gallerySlides.style.transform = `translateX(${offset}%)`;
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % galleryImages.length;
+    showImage(currentIndex);
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + galleryImages.length) % galleryImages.length;
+    showImage(currentIndex);
+}
+
+nextButton.addEventListener('click', nextImage);
+prevButton.addEventListener('click', prevImage);
+
+setInterval(nextImage, 5000); // Cambia de imagen automáticamente cada 5 segundos
